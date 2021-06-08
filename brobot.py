e60f21c20cbdc8ec27d750a1b2f7b249a3fc8119
Samuel <77265662+ReadTheMango@users.noreply.github.com>
2021-06-08 15:47:29 +1000

    completed brobot project
    updated brobot, pay attention to the test

---
+++ brobot.py
@@ -0,0 +1,60 @@
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me, what is your name?')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    print('''What is it that you hack into in all the movies?
+    1. Your mum
+    2. The locker room
+    3. All your base
+    4. The mainframe''')
+    flag = 0
+    while flag == 0:
+        answer = input()
+        if answer == "4":
+            print('Completed, have a nice day!')
+            flag =+ 1
+        else: print('Please, try again')
+
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Brobot', '2021')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
